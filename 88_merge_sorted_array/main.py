2166f97cbd6c1e94b02d6560658ecc40350f5b8a
Ozzy G. Castillo <castillo.oswaldo@outlook.com>
2025-04-26 00:32:27 -0400

    Logger added and problem description

---
+++ 88_merge_sorted_array/main.py
@@ -0,0 +1,30 @@
+import os
+from typing import List
+import utilityFunctions.ConsoleLogger  as consoleLog
+import utilityFunctions.FileLogger as fileLog
+
+
+def merge(nums1: List[int], m: int, nums2: List[int], n: int):
+    # log = consoleLogger().Logger get_logger()
+    log = fileLog.FileLogger.get_logger()
+    log.info(f"Executing script: {os.path.basename(__file__)}")
+    auxNums: List[int]
+    if n > 0:
+        for i in range(m, m + n):
+            log.info("i = "+ str(i) )
+            if nums1[i] < nums2[i]:
+                nums1.insert(i, nums2[i])
+            else:
+                nums1.insert(i, nums2[i+1])
+
+if __name__ == '__main__':
+
+    log = fileLog.FileLogger.get_logger()
+    log.info(f"Executing script: {os.path.basename(__file__)}")
+    nums1 = [1, 2, 3, 0, 0, 0]
+    m = 3
+    nums2 = [2, 5, 6]
+    n = 3
+    merge(nums1, m, nums2, n)
+    print(nums1)
+
